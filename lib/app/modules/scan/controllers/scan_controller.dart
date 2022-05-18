5e12dbeae2a45c92cdc2659463c80cea875ac40a
neojarma <neojarmawijaya@gmail.com>
2022-05-18 13:50:17 +0700

    feat: qr scan

--- lib/app/modules/scan/controllers/scan_controller.dart
+++ lib/app/modules/scan/controllers/scan_controller.dart
@@ -1,3 +1,27 @@
 import 'package:get/get.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScanController extends GetxController {}
+class ScanController extends GetxController {
+  late MobileScannerController mobileScannerController;
+  String? barcodeValue;
+
+  @override
+  void onInit() {
+    mobileScannerController = MobileScannerController();
+
+    super.onInit();
+  }
+
+  dynamic onDetect(
+      Barcode barcode, MobileScannerArguments? mobileScannerArguments) {
+    if (barcode.rawValue == null) {
+      Get.snackbar('failed', 'failed');
+    } else {
+      Get.snackbar('Barcode Found', 'barcode found');
+      final String code = barcode.rawValue!;
+      barcodeValue = code;
+      // Get.toNamed(Routes.HOME);
+      // mobileScannerController.dispose();
+    }
+  }
+}
--- lib/app/modules/scan/views/scan_view.dart
+++ lib/app/modules/scan/views/scan_view.dart
@@ -1,17 +1,65 @@
+import 'dart:ui';
+
+import 'package:digi_kot/app/core/themes/text_themes.dart';
 import 'package:digi_kot/app/core/values/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
 import '../controllers/scan_controller.dart';
 
 class ScanView extends GetView<ScanController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: darkBlue,
-        height: Get.height,
-        clipBehavior: Clip.none,
+      body: Stack(
+        children: [
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: Container(
+              color: darkBlue,
+              height: Get.height,
+              clipBehavior: Clip.none,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: MobileScanner(
+                          fit: BoxFit.cover,
+                          allowDuplicates: false,
+                          controller: controller.mobileScannerController,
+                          onDetect: controller.onDetect,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Scan QR Code',
+                    style: homePageTexStyle.copyWith(color: Colors.white),
+                  ),
+                  Text(
+                    'Letakkan QR Code pada Scan Area',
+                    style: homePageTexStyle.copyWith(
+                        color: Colors.white, fontSize: 13),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
